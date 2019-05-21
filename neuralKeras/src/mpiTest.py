d4ac179d0a49ee993c538c5104c916f3b086cba9
causelovem <rat.taurus@gmail.com>
2019-05-21 23:27:29 +0300

    some improvements

--- neuralKeras/src/mpiTest.py
+++ neuralKeras/src/mpiTest.py
@@ -14,7 +14,7 @@ from keras.utils import plot_model, normalize, np_utils
 # from keras.regularizers import l2
 
 from mpi4py import MPI as mpi
-# import queue as qu
+import queue as qu
 
 np.set_printoptions(threshold=np.nan)
 
@@ -22,89 +22,93 @@ comm = mpi.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-matrixList = []
-# matrixFiles = os.listdir("./matrix")
-matrixFiles = os.listdir("./pred/matrix")
-matrixFiles.sort(key=lambda x: int(x[6:]))
+if (rank == 0):
+    matrixList = []
+    # matrixFiles = os.listdir("./matrix")
+    matrixFiles = os.listdir("./pred/matrix")
+    matrixFiles.sort(key=lambda x: int(x[6:]))
 
-persent = -1
-print('> Readind matrix data...')
-for file in matrixFiles:
-    persent += 1
-    print(str(round(persent * 100 / len(matrixFiles), 1)) + '%', end='')
-    print('\r', end='')
+    persent = -1
+    print('> Readind matrix data...')
+    for file in matrixFiles:
+        persent += 1
+        print(str(round(persent * 100 / len(matrixFiles), 1)) + '%', end='')
+        print('\r', end='')
 
-    # fileIn = open("./matrix/" + file, "r")
-    fileIn = open("./pred/matrix/" + file, "r")
+        # fileIn = open("./matrix/" + file, "r")
+        fileIn = open("./pred/matrix/" + file, "r")
 
-    matrix = fileIn.readlines()
-    dim = len(matrix)
+        matrix = fileIn.readlines()
+        dim = len(matrix)
 
-    for i in range(dim):
-        matrix[i] = matrix[i][:-2].split(' ')
-        # for j in range(len(matrix[i])):
-        #     matrix[i][j] = int(matrix[i][j])
+        for i in range(dim):
+            matrix[i] = matrix[i][:-2].split(' ')
+            # for j in range(len(matrix[i])):
+            #     matrix[i][j] = int(matrix[i][j])
 
-        pairList = []
-        for j in range(dim):
-            pairList.append((j, int(matrix[i][j])))
+            pairList = []
+            for j in range(dim):
+                pairList.append((j, int(matrix[i][j])))
 
-        pairList.sort(key=lambda x: x[1])
+            pairList.sort(key=lambda x: x[1])
 
-        for j in range(dim):
-            if (pairList[j][1] == 0):
-                matrix[i][pairList[j][0]] = 0
-            else:
-                matrix[i][pairList[j][0]] = j + 1
+            for j in range(dim):
+                if (pairList[j][1] == 0):
+                    matrix[i][pairList[j][0]] = 0
+                else:
+                    matrix[i][pairList[j][0]] = j + 1
 
-    tmp = np.array(matrix)
-    tmp = np.expand_dims(tmp, axis=2)
-    matrixList.append(tmp)
+        tmp = np.array(matrix)
+        tmp = np.expand_dims(tmp, axis=2)
+        matrixList.append(tmp)
 
-    fileIn.close()
+        fileIn.close()
 
-matrixVec = np.array(matrixList)
-matrixDim = int(matrixVec.shape[1])
-numOfSet = int(matrixVec.shape[0])
+    matrixVec = np.array(matrixList)
+    matrixDim = int(matrixVec.shape[1])
+    # numOfSet = int(matrixVec.shape[0])
 
-# print(matrixVec[0])
-# matrixVec = normalize(matrixVec, 2)
-# print(matrixVec[0])
+    # print(matrixVec[0])
+    # matrixVec = normalize(matrixVec, 2)
+    # print(matrixVec[0])
 
-print(matrixVec.shape)
+    print(matrixVec.shape)
 
 # sys.exit(0)
 
-mappingList = []
-# mappingFiles = os.listdir("./mapping")
-mappingFiles = os.listdir("./pred/test")
-mappingFiles.sort(key=lambda x: int(x[7:]))
+# mappingList = []
+# # mappingFiles = os.listdir("./mapping")
+# mappingFiles = os.listdir("./pred/test")
+# mappingFiles.sort(key=lambda x: int(x[7:]))
 
-persent = -1
-print('> Readind mapping data...')
-for file in mappingFiles:
-    persent += 1
-    print(str(round(persent * 100 / len(mappingFiles), 1)) + '%', end='')
-    print('\r', end='')
+# persent = -1
+# print('> Readind mapping data...')
+# for file in mappingFiles:
+#     persent += 1
+#     print(str(round(persent * 100 / len(mappingFiles), 1)) + '%', end='')
+#     print('\r', end='')
 
-    # fileIn = open("./mapping/" + file, "r")
-    fileIn = open("./pred/test/" + file, "r")
+#     # fileIn = open("./mapping/" + file, "r")
+#     fileIn = open("./pred/test/" + file, "r")
 
-    mapping = fileIn.readline()
-    mappingList.append(int(mapping[:-1]))
+#     mapping = fileIn.readline()
+#     mappingList.append(int(mapping[:-1]))
 
-    fileIn.close()
+#     fileIn.close()
 
-mappingVec = np.array(mappingList)
-# numClass = np.max(mappingVec) + 1
-numClass = 7
-mappingVec = np_utils.to_categorical(mappingVec, numClass)
+# mappingVec = np.array(mappingList)
+# # numClass = np.max(mappingVec) + 1
+# numClass = 7
+# mappingVec = np_utils.to_categorical(mappingVec, numClass)
 
 # sys.exit(0)
 
 # print(mappingVec[0])
 # mappingVec = normalize(mappingVec, 1)
 # print(mappingVec[0])
+if (rank != 0):
+    matrixVec = []
+lenMatrixVec = comm.bcast(len(matrixVec), root=0)
 
 print('> Preparing for train...')
 lenMapStr = 4
@@ -119,11 +123,19 @@ if 1 == 1:
     img_input = Input(shape=[256, 256, 1])
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv3')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv4')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv5')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv6')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
     x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
     x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv3')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv4')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv5')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv6')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
@@ -131,6 +143,8 @@ if 1 == 1:
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv5')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv6')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
@@ -138,6 +152,8 @@ if 1 == 1:
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv5')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv6')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
@@ -145,11 +161,15 @@ if 1 == 1:
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv5')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv6')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     x = Flatten(name='flatten')(x)
     x = Dense(4096, activation='relu', name='fc1')(x)
     x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(4096, activation='relu', name='fc3')(x)
+    x = Dense(4096, activation='relu', name='fc4')(x)
     x = Dense(7, activation='softmax', name='predictions')(x)
 
     model = Model(inputs=img_input, outputs=x)
@@ -191,7 +211,7 @@ comm.Barrier()
 #     if len(weig) > 0:
 #         model.get_layer(index=(i + 1)).set_weights([weig, bias])
 
-# que = qu.Queue()
+que = qu.Queue()
 
 if 0 == 1:
     if (rank == 0):
@@ -230,10 +250,42 @@ if 0 == 1:
 persent = -1
 print('> Predict on test data...')
 t1 = mpi.Wtime()
-lenMatrixVec = len(matrixVec)
+# lenMatrixVec = len(matrixVec)
+# for i in range(lenMatrixVec):
+#     persent += 1
+#     print(str(round(persent * 100 / lenMatrixVec, 1)) + '%', end='')
+#     print('\r', end='')
+
+#     if (rank == 0):
+#         # pred = modelDiv.predict(matrixVec[i:i + 1])
+#         # comm.send(pred, dest=1, tag=0)
+#         # req = comm.isend(modelDiv.predict(matrixVec[i:i + 1]), dest=1, tag=0)
+#         req = comm.send(modelDiv.predict(matrixVec[i:i + 1]), dest=1, tag=0)
+#         # print(rank, 'isend')
+#         # if (i == lenMatrixVec - 1):
+#         #     req.wait()
+#         #     print(rank, 'send')
+#     elif (rank != size - 1):
+#         pred = modelDiv.predict(comm.recv(source=(rank - 1), tag=0))
+#         # print(rank, 'recv')
+#         comm.send(pred, dest=(rank + 1), tag=0)
+#         # print(rank, 'isend')
+#     elif (rank == size - 1):
+#         # tmp = comm.recv(source=0, tag=0)
+#         # pred = modelDiv.predict(tmp)
+#         pred = modelDiv.predict(comm.recv(source=(rank - 1), tag=0))
+#         # print(rank, 'recv')
+#         # print("./pred/prediction/mapping" + str(i + 1) + "Pred")
+#         # fileOut = open("./pred/prediction/mapping" + str(i + 1) + "Pred", "w")
+
+#         # fileOut.write(str(np.where(pred == pred.max())[1][0]))
+#         # fileOut.write('\n')
+#         # # print(str(np.where(pred == pred.max())))
+#         # # print(str(np.where(pred == pred.max())[1][0]))
+#         # fileOut.close()
 for i in range(lenMatrixVec):
     persent += 1
-    print(str(round(persent * 100 / len(matrixVec), 1)) + '%', end='')
+    print(str(round(persent * 100 / lenMatrixVec, 1)) + '%', end='')
     print('\r', end='')
 
     if (rank == 0):
@@ -241,20 +293,20 @@ for i in range(lenMatrixVec):
         # comm.send(pred, dest=1, tag=0)
         # req = comm.isend(modelDiv.predict(matrixVec[i:i + 1]), dest=1, tag=0)
         req = comm.send(modelDiv.predict(matrixVec[i:i + 1]), dest=1, tag=0)
-        print(rank, 'isend')
+        # print(rank, 'isend')
         # if (i == lenMatrixVec - 1):
         #     req.wait()
         #     print(rank, 'send')
     elif (rank != size - 1):
         pred = modelDiv.predict(comm.recv(source=(rank - 1), tag=0))
-        print(rank, 'recv')
+        # print(rank, 'recv')
         comm.send(pred, dest=(rank + 1), tag=0)
-        print(rank, 'isend')
+        # print(rank, 'isend')
     elif (rank == size - 1):
         # tmp = comm.recv(source=0, tag=0)
         # pred = modelDiv.predict(tmp)
         pred = modelDiv.predict(comm.recv(source=(rank - 1), tag=0))
-        print(rank, 'recv')
+        # print(rank, 'recv')
         # print("./pred/prediction/mapping" + str(i + 1) + "Pred")
         # fileOut = open("./pred/prediction/mapping" + str(i + 1) + "Pred", "w")
 
