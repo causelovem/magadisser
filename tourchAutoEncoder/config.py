48e9ab76100895821a60eccfd5bcd0e2792b42b8
causelovem <rat.taurus@gmail.com>
2020-03-27 22:12:47 +0300

    sync

--- tourchAutoEncoder/config.py
+++ tourchAutoEncoder/config.py
@@ -4,7 +4,7 @@ import platform as plt
 sys = plt.system()
 print(sys)
 
-fileDir = '/mnt/ssd1/prog/pdbFiles'
+fileDir = '/mnt/ssd1/prog/pdbFilesGdata'
 if sys == 'Windows':
     fileDir = 'F:/prog/magadisser/tourchAutoEncoder/data/pdbFilesGdata'
 
